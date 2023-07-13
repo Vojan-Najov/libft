06372b873eae965d6c1c89f76a5c2df69bbb81c6
Vojan-Najov <serejah.mashichev@gmail.com>
2023-07-13 13:27:01 +0300

    Add ft_lstclear.c

---
+++ ft_lstclear.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccartman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/30 00:05:42 by ccartman          #+#    #+#             */
+/*   Updated: 2021/05/01 18:25:39 by ccartman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*ptr;
+	t_list	*tmp;
+
+	ptr = *lst;
+	while (ptr)
+	{
+		(*del)(ptr->content);
+		tmp = ptr;
+		ptr = ptr->next;
+		free(tmp);
+	}
+	*lst = NULL;
+}
